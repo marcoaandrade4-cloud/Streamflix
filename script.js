f8ef494749921aa7109890e7873cd38a0b342910
marcoaandrade4-cloud <marcoaandrade4@gmail.com>
2026-05-01 22:15:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,179 +1,178 @@
-const senhaADM="1234";
-let db=JSON.parse(localStorage.getItem("db")||"[]");
-let idiomasTemp={};
+const senhaADM = "1234";
+let db = JSON.parse(localStorage.getItem("db") || "[]");
 
-/* NAV */
-function abrirADM(){location.href="adm.html";}
+/* BOTÃO ADM (CORRIGIDO) */
+document.addEventListener("DOMContentLoaded", ()=>{
+  let btn = document.getElementById("btnADM");
+  if(btn){
+    btn.addEventListener("click", ()=>{
+      window.location.href = "adm.html";
+    });
+  }
+});
 
 /* LOGIN */
 function login(){
-if(senha.value===senhaADM){
-loginBox.style.display="none";
-painel.style.display="block";
-atualizarLista();
-}else alert("Senha errada");
+  if(document.getElementById("senha").value === senhaADM){
+    loginBox.style.display="none";
+    painel.style.display="block";
+    atualizarLista();
+  } else alert("Senha errada");
 }
 
 /* DB */
-function salvar(){localStorage.setItem("db",JSON.stringify(db));}
+function salvar(){localStorage.setItem("db", JSON.stringify(db));}
 
 /* CRIAR */
 function criarSerie(){
-db.push({
-nome:nomeSerie.value,
-capa:capaSerie.value,
-sinopse:sinopseSerie.value,
-ano:anoSerie.value,
-tipo:tipo.value,
-temporadas:[]
-});
-salvar();atualizarLista();
+  db.push({nome:nomeSerie.value,capa:capaSerie.value,temporadas:[]});
+  salvar(); atualizarLista();
 }
 
 /* LISTA */
 function atualizarLista(){
-listaSeries.innerHTML="";
-db.forEach((s,i)=>listaSeries.innerHTML+=`<option value="${i}">${s.nome}</option>`);
+  listaSeries.innerHTML="";
+  db.forEach((s,i)=>{
+    listaSeries.innerHTML+=`<option value="${i}">${s.nome}</option>`;
+  });
 }
 
 /* TEMP */
 function addTemp(){
-db[listaSeries.value].temporadas.push({episodios:[]});
-salvar();
+  db[listaSeries.value].temporadas.push({episodios:[]});
+  salvar();
 }
 
-/* MULTI IDIOMA */
-function addIdioma(){
-idiomasTemp[lang.value]=link.value;
-alert("Idioma adicionado");
-}
-
-/* ADD EP */
+/* EP */
 function addEp(){
-let s=db[listaSeries.value];
-let t=tempNum.value;
+  let i = listaSeries.value;
+  let t = tempNum.value;
 
-if(s.tipo==="filme"){
-t=0;
-if(!s.temporadas[0]) s.temporadas[0]={episodios:[]};
-}
+  if(!db[i] || !db[i].temporadas[t]) return alert("Temporada inválida");
 
-db.forEach(s=>s.temporadas.forEach(t=>t.episodios.forEach(ep=>ep.novo=false)));
+  // mantém lógica original do novo episódio
+  db.forEach(s=>{
+    s.temporadas.forEach(t=>{
+      t.episodios.forEach(ep=>ep.novo=false);
+    });
+  });
 
-let ep={
-titulo:tituloEpADM.value,
-capa:capaEpADM.value,
-links:{...idiomasTemp},
-novo:novoEp.checked
-};
+  let ep={
+    titulo:tituloEpADM.value,
+    links:{}
+  };
 
-s.temporadas[t].episodios.push(ep);
+  ep.links[lang.value]=link.value;
 
-idiomasTemp={};
+  if(novoEp.checked) ep.novo=true;
 
-salvar();
-alert("Salvo!");
+  db[i].temporadas[t].episodios.push(ep);
+
+  salvar();
+  alert("Salvo!");
 }
 
 /* HOME */
 function carregarHome(){
-let grid=document.getElementById("grid");
-if(!grid) return;
+  let grid=document.getElementById("grid");
+  if(!grid) return;
 
-grid.innerHTML="";
+  grid.innerHTML="";
 
-db.forEach((s,i)=>{
-grid.innerHTML+=`
-<div class="card" onclick="abrirSerie(${i})">
-<img src="${s.capa}">
-</div>`;
-});
+  db.forEach((s,i)=>{
+    grid.innerHTML+=`
+      <div class="card" onclick="abrirSerie(${i})">
+        <img src="${s.capa}">
+      </div>`;
+  });
 
-let destaque;
-db.forEach(s=>s.temporadas.forEach(t=>t.episodios.forEach(ep=>{
-if(ep.novo)destaque=ep;
-})));
+  let destaque;
+  db.forEach(s=>{
+    s.temporadas.forEach(t=>{
+      t.episodios.forEach(ep=>{
+        if(ep.novo) destaque=ep;
+      });
+    });
+  });
 
-if(destaque){
-epNome.innerText=destaque.titulo;
-epInfo.innerText="Novo episódio disponível";
-btnAssistir.onclick=()=>{
-localStorage.setItem("ep",JSON.stringify(destaque));
-location.href="player.html";
-};
-}
+  if(destaque){
+    epNome.innerText=destaque.titulo;
+
+    btnAssistir.onclick=()=>{
+      localStorage.setItem("ep",JSON.stringify(destaque));
+      location.href="player.html";
+    };
+  }
 }
 
 /* SERIE */
 function abrirSerie(i){
-localStorage.setItem("serieIndex",i);
-location.href="serie.html";
+  localStorage.setItem("serieIndex",i);
+  location.href="serie.html";
 }
 
 function carregarSeriePage(){
-let i=localStorage.getItem("serieIndex");
-if(i==null)return;
+  let i=localStorage.getItem("serieIndex");
+  if(i==null) return;
 
-let s=db[i];
+  let s=db[i];
+  titulo.innerText=s.nome;
 
-titulo.innerText=s.nome;
-sinopse.innerText=s.sinopse;
-ano.innerText=s.ano;
+  s.temporadas.forEach((t,ti)=>{
+    let b=document.createElement("button");
+    b.innerText="Temporada "+(ti+1);
 
-s.temporadas.forEach((t,ti)=>{
-let b=document.createElement("button");
-b.innerText="Temporada "+(ti+1);
+    b.onclick=()=>{
+      episodios.innerHTML="";
+      t.episodios.forEach(ep=>{
+        let e=document.createElement("button");
+        e.innerText=ep.titulo;
 
-b.onclick=()=>{
-episodios.innerHTML="";
-t.episodios.forEach(ep=>{
-let e=document.createElement("div");
-e.innerHTML=`
-<img src="${ep.capa}" width="120">
-<p>${ep.titulo}</p>
-`;
-e.onclick=()=>{
-localStorage.setItem("ep",JSON.stringify(ep));
-location.href="player.html";
-};
-episodios.appendChild(e);
-});
-};
+        e.onclick=()=>{
+          localStorage.setItem("ep",JSON.stringify(ep));
+          location.href="player.html";
+        };
 
-temporadas.appendChild(b);
-});
+        episodios.appendChild(e);
+      });
+    };
+
+    temporadas.appendChild(b);
+  });
 }
 
 /* PLAYER */
 function carregarPlayer(){
-let ep=JSON.parse(localStorage.getItem("ep"));
-if(!ep)return;
+  let ep=JSON.parse(localStorage.getItem("ep"));
+  if(!ep) return;
 
-tituloEp.innerText=ep.titulo;
-capaEp.src=ep.capa;
+  tituloEp.innerText=ep.titulo;
+  idiomas.innerHTML="";
 
-Object.keys(ep.links).forEach(l=>{
-let b=document.createElement("button");
-b.innerText=l;
+  Object.keys(ep.links).forEach(lang=>{
+    let b=document.createElement("button");
+    b.innerText=lang;
 
-b.onclick=()=>{
-let url=ep.links[l];
+    b.onclick=()=>{
+      let url=ep.links[lang];
 
-if(url.includes("youtube")){
-let id=url.includes("v=")?url.split("v=")[1]:url.split("/").pop();
-player.src="https://www.youtube.com/embed/"+id;
-}
-else if(url.includes("drive")){
-let id=url.split("/d/")[1]?.split("/")[0];
-player.src="https://drive.google.com/file/d/"+id+"/preview";
-}
-else player.src=url;
-};
+      if(url.includes("youtube")){
+        let id=url.includes("v=")?url.split("v=")[1].split("&")[0]:url.split("/").pop();
+        player.src="https://www.youtube.com/embed/"+id;
+      }
+      else if(url.includes("drive")){
+        let id=url.split("/d/")[1]?.split("/")[0];
+        player.src="https://drive.google.com/file/d/"+id+"/preview";
+      }
+      else{
+        player.src=url;
+      }
+    };
 
-idiomas.appendChild(b);
-});
+    idiomas.appendChild(b);
+  });
 
-idiomas.querySelector("button")?.click();
+  idiomas.querySelector("button")?.click();
 }
 
 /* AUTO */
