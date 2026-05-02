e5e5319519be32e4c308f202f3aa55b60a6172ae
marcoaandrade4-cloud <marcoaandrade4@gmail.com>
2026-05-02 13:55:55 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBkpOKakidlZ2WVZNBI9oEFHEjs1EqCt78",
+  authDomain: "streamflix-62461.firebaseapp.com",
+  projectId: "streamflix-62461",
+  storageBucket: "streamflix-62461.appspot.com",
+  messagingSenderId: "894473429433",
+  appId: "1:894473429433:web:51a0215903fbb277305668",
+  measurementId: "G-YC422EKENH"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
