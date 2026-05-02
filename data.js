d0bc8b38a458e5c5f16827d0e8e8b286fb3a4f53
marcoaandrade4-cloud <marcoaandrade4@gmail.com>
2026-05-02 14:44:09 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+const DB = {
+  temporadas: [
+    {
+      id: 1,
+      nome: "1ª Temporada",
+      capa: "img/t1.jpg",
+      episodios: [
+        {
+          id: 1,
+          titulo: "Origens - Parte 1",
+          descricao: "Como tudo começou...",
+          thumb: "img/ep1.jpg",
+          video: "https://www.youtube.com/embed/SEU_VIDEO"
+        },
+        {
+          id: 2,
+          titulo: "Coração de Pedra",
+          descricao: "Continuação da história...",
+          thumb: "img/ep2.jpg",
+          video: "https://www.youtube.com/embed/SEU_VIDEO"
+        }
+      ]
+    }
+  ]
+};
